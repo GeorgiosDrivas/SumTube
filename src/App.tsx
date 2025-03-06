01e83d70123fe557f7291bcb3bf0354a5413a5d3
Georgios Drivas <drivasgeorgios0@gmail.com>
2025-03-06 12:19:51 +0200

    Refactor landing page

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,10 @@ export default function App() {
           <h1>
             Youtube video <span>Summarizer</span>
           </h1>
-          <p>
-            SumTube summarizes YouTube videos by generating concise text
-            summaries using AI. <br />
-            Simply paste a video link and get a quick, readable summary!
-          </p>
         </section>
         <section id="form">
           <input type="text" placeholder="Youtube video link" />
-          <button>Summarize video</button>
+          <button>Summarize</button>
         </section>
       </main>
     </>
